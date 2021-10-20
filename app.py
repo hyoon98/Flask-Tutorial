eb98b0cc33416f84b737d098e2e5a7e9c7875f5b
hya62 <hansoo_yoon@sfu.ca>
2021-10-20 15:34:57 -0700

    added update page, finished app

--- app.py
+++ app.py
@@ -45,5 +45,19 @@ def delete(id):
         return 'There was issue deleting task'
 
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was issue updating task"
+    else:
+        return(render_template('update.html', task=task_to_update))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
